941a2a0390df0ed4a64d77995adf29dc5a8fceb3
name <@DESKTOP-A9J5UUF>
2021-10-14 13:20:01 +0900

    a

---
+++ zombie/src/Main.java
@@ -0,0 +1,8 @@
+import controller.Game;
+
+public class Main {
+	public static void main(String[] args) {
+		Game game = Game.getInstance();
+		game.run();
+	}
+}
---
+++ zombie/src/controller/Game.java
@@ -0,0 +1,14 @@
+package controller;
+
+
+public class Game {
+	private static Game instance = new Game();
+	private Game() {};
+	public static Game getInstance() {
+		return instance;
+	}
+	
+	public void run(){
+		
+	}
+}
